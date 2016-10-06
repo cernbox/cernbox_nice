37c7b92bfef7af10b0ccf94c95c88d1202bd470e
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2016-10-06 10:36:57 +0200

    Init

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ownCloud - cernboxnice
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Hugo Gonzalez Labrador <hugo.gonzalez.labrador@cern.ch>
+ * @copyright Hugo Gonzalez Labrador 2016
+ */
+
+require_once __DIR__ . '/../../../tests/bootstrap.php';
+require_once __DIR__ . '/../appinfo/autoload.php';
